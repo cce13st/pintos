fbffe20f99c299a47e8c106e348c3cc925841c30
ccelest <ccelest@kaist.ac.kr>
2013-09-13 21:21:09 +0900

    Alarmclock start

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -101,6 +101,7 @@ timer_sleep (int64_t ticks)
   ASSERT (intr_get_level () == INTR_ON);
   while (timer_elapsed (start) < ticks) 
     thread_yield ();
+    /* modify this part without busy waiting */
 }
 
 /* Suspends execution for approximately MS milliseconds. */
