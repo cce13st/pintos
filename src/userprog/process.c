03926aae706c0f9986e9a421e9e3567014e94fd2
ccelest <ccelest@kaist.ac.kr>
2013-10-07 22:10:42 +0900

    반정도 성공

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -31,7 +31,6 @@ process_execute (const char *file_name)
   char *fn_copy;
   tid_t tid;
 
-  printf ("execute\n");
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
@@ -94,7 +93,6 @@ start_process (void *f_name)
   struct intr_frame if_;
   bool success;
   int i = 0;
-  printf ("start_process\n");
 
   fp = file_name;
   while (*fp != ' ' && *fp != 0){
@@ -111,18 +109,13 @@ start_process (void *f_name)
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  printf ("%s %d\n", load_name, strlen(load_name));
-  printf ("%s %d\n", file_name, strlen(file_name));
   success = load (load_name, &if_.eip, &if_.esp);
-  printf ("start_process\n");
 
-  printf ("load complete\n");
   /* If load failed, quit. */
   if (!success) 
     thread_exit ();
 
   if_.esp = args_passing (if_.esp, file_name);
-  hex_dump ((int) if_.esp, if_.esp, 40, true);  
   palloc_free_page (file_name); 
  
   /* Start the user process by simulating a return from an
@@ -274,24 +267,19 @@ load (const char *file_name, void (**eip) (void), void **esp)
   bool success = false;
   int i;
 
-  printf ("load start\n");
   /* Allocate and activate page directory. */
   t->pagedir = pagedir_create ();
-  printf ("load2\n");
   if (t->pagedir == NULL) 
     goto done;
   process_activate ();
 
-  printf ("%s\n", file_name);
   /* Open executable file. */
   file = filesys_open (file_name);
-  printf ("load3\n");
   if (file == NULL) 
     {
       printf ("load: %s: open failed\n", file_name);
       goto done; 
     }
-  printf ("load3\n");
 
   /* Read and verify executable header. */
   if (file_read (file, &ehdr, sizeof ehdr) != sizeof ehdr
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,7 +6,6 @@
 
 static void syscall_handler (struct intr_frame *);
 static void syscall_exit (struct intr_frame *);
-static void syscall_halt (struct intr_frame *);
 static void syscall_write (struct intr_frame *);
 
 void
@@ -19,24 +18,14 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   int syscall_n = *(int *)(f->esp);
-  printf ("syscall - %d\n", syscall_n);
-  hex_dump ((int) f->esp, f->esp, 128, true);
     
   /*  Switch-case for system call number */
   switch (syscall_n){
-    case SYS_HALT:
-      syscall_halt (f);
     case SYS_EXIT:
       syscall_exit (f);
     case SYS_WRITE:  
       syscall_write (f);
   }
-}
-
-static void
-syscall_halt (struct intr_frame *f)
-{
-  power_off ();
 }
 
 static void
@@ -51,12 +40,11 @@ syscall_exit (struct intr_frame *f)
 static void
 syscall_write (struct intr_frame *f)
 {
-  int fd;
+  int fd = *(int *)(f->esp + 4);
   void *buffer;
+  memcpy (&buffer, f->esp+8, sizeof (unsigned));
   unsigned size;
-  memcpy (&fd, f->esp+1, sizeof (int));
-  memcpy (&buffer, f->esp+2, sizeof (void *));
-  memcpy (&size, f->esp+3, sizeof (unsigned));
+  memcpy (&size, f->esp+12, sizeof (unsigned));
   if (fd == 1)
     putbuf (buffer, size);
   f->eax = size;
