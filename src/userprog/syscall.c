ff1d32885e98b7b0909576330929e4f2192b4a8b
ccelest <ccelest@kaist.ac.kr>
2013-10-07 17:21:01 +0400

    sys_halt

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,7 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static void syscall_halt (struct intr_frame *);
 static void syscall_exit (struct intr_frame *);
 static void syscall_write (struct intr_frame *);
 
@@ -21,11 +22,19 @@ syscall_handler (struct intr_frame *f UNUSED)
     
   /*  Switch-case for system call number */
   switch (syscall_n){
+    case SYS_HALT:
+      syscall_exit (f);
     case SYS_EXIT:
       syscall_exit (f);
     case SYS_WRITE:  
       syscall_write (f);
   }
+}
+
+static void
+syscall_halt (struct intr_frame *f)
+{
+  power_off ();
 }
 
 static void
