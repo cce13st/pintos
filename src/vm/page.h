af49d0e99029f4670efb5079f9216f4ef4e793b6
bsy100 <bsy.100@kaist.ac.kr>
2013-10-29 19:00:19 +0300

    page.c & page.h modify

--- src/vm/page.h
+++ src/vm/page.h
@@ -1,3 +1,8 @@
+#ifndef SPAGE_H_
+#include "threads/vaddr.h"
+#include "threads/synch.h"
+#include <hash.h>
+
 struct spt_entry
 {
 	bool pinned;
@@ -9,9 +14,12 @@ struct spt_entry
 	struct hash_elem hash_elem;
 };
 
+struct lock spage_lock;
 struct hash spage_hash;
 
 void spage_init (void);
 void spage_insert ();
 void spage_remove ();
 
+
+#endif /* vm/page.h end */
