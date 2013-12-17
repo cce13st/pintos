8e03afa8270d234663d2535ac59d9c354e93bc66
bsy100 <bsy.100@kaist.ac.kr>
2013-12-17 17:11:48 +0900

    Update directory.c

--- src/filesys/directory.c
+++ src/filesys/directory.c
@@ -242,12 +242,12 @@ dir_exist_parent (struct dir *dir, struct inode *inode)
 	while (parent != base) {
 		if (parent->inode == inode){
 			return true;
-			}
+		}
 
 		struct inode *temp;
 		if (!dir_lookup (parent, "..", &temp)){
 			return false;
-			}
+		}
 		prev = parent;
 		parent = dir_open (temp);
 		dir_close (prev);
@@ -276,23 +276,6 @@ dir_readdir (struct dir *dir, char name[NAME_MAX + 1])
    	}
   return false;
 }
-
-
-/* Check if directory is empty. */
-/*bool
-dir_is_empty (struct dir *dir)
-{
-	struct dir_entry e;
-	off_t ofs;
-
-	for (ofs = 0; inode_read_at (dir->inode, &e, sizeof e, ofs) == sizeof e; ofs+= sizeof e)
-	{
-		if (e.in_use)	// If inode sector is not 0, there exists some file(or directory)
-			return false;
-	}	
-	return true;
-}
-*/
 
 /*make get_directory method to implement easily */
 struct dir*
